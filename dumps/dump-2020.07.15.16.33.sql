93cb2fe6e6a25245d49768b26497d33687b4ae44
malletgaetan <mallet.gaetan@hotmail.fr>
2020-07-15 07:35:22 -0700

    ajout dump de StateOfMedia

---
+++ dumps/dump-2020.07.15.16.33.sql
@@ -0,0 +1,473 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: mediatheque
+-- ------------------------------------------------------
+-- Server version	8.0.20-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `id` int NOT NULL,
+  `edition` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `page_nb` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_CBE5A331BF396750` FOREIGN KEY (`id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `borrow`
+--
+
+DROP TABLE IF EXISTS `borrow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `borrow` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `stockable_media_copy_id` int NOT NULL,
+  `return_media_state_id` int DEFAULT NULL,
+  `borrow_date` datetime NOT NULL,
+  `expiry_date` datetime NOT NULL,
+  `return_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_55DBA8B07597D3FE` (`member_id`),
+  KEY `IDX_55DBA8B02CA63995` (`stockable_media_copy_id`),
+  KEY `IDX_55DBA8B09DEFAEC9` (`return_media_state_id`),
+  CONSTRAINT `FK_55DBA8B02CA63995` FOREIGN KEY (`stockable_media_copy_id`) REFERENCES `stockable_media_copy` (`id`),
+  CONSTRAINT `FK_55DBA8B07597D3FE` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
+  CONSTRAINT `FK_55DBA8B09DEFAEC9` FOREIGN KEY (`return_media_state_id`) REFERENCES `state_of_media` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrow`
+--
+
+LOCK TABLES `borrow` WRITE;
+/*!40000 ALTER TABLE `borrow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `borrow` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `digital_media`
+--
+
+DROP TABLE IF EXISTS `digital_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `digital_media` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `media_id` int NOT NULL,
+  `url` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `path` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8C16F48CEA9FDD75` (`media_id`),
+  CONSTRAINT `FK_8C16F48CEA9FDD75` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `digital_media`
+--
+
+LOCK TABLES `digital_media` WRITE;
+/*!40000 ALTER TABLE `digital_media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `digital_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `film`
+--
+
+DROP TABLE IF EXISTS `film`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `film` (
+  `id` int NOT NULL,
+  `original_title` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `synopsis` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `duration` int DEFAULT NULL,
+  `director` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `actors` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_8244BE22BF396750` FOREIGN KEY (`id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `film`
+--
+
+LOCK TABLES `film` WRITE;
+/*!40000 ALTER TABLE `film` DISABLE KEYS */;
+/*!40000 ALTER TABLE `film` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `media` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `media_type_id` int NOT NULL,
+  `name` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `media_type` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_6A2CA10CA49B0ADA` (`media_type_id`),
+  CONSTRAINT `FK_6A2CA10CA49B0ADA` FOREIGN KEY (`media_type_id`) REFERENCES `media_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media`
+--
+
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media_type`
+--
+
+DROP TABLE IF EXISTS `media_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `media_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `borrow_duration` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media_type`
+--
+
+LOCK TABLES `media_type` WRITE;
+/*!40000 ALTER TABLE `media_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_by_staff_id` int DEFAULT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address1` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address2` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `zipcode` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_70E4FA78D972133` (`created_by_staff_id`),
+  CONSTRAINT `FK_70E4FA78D972133` FOREIGN KEY (`created_by_staff_id`) REFERENCES `staff` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migration_versions`
+--
+
+DROP TABLE IF EXISTS `migration_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `migration_versions` (
+  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migration_versions`
+--
+
+LOCK TABLES `migration_versions` WRITE;
+/*!40000 ALTER TABLE `migration_versions` DISABLE KEYS */;
+INSERT INTO `migration_versions` VALUES ('20200715130024','2020-07-15 13:00:32');
+/*!40000 ALTER TABLE `migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `music`
+--
+
+DROP TABLE IF EXISTS `music`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `music` (
+  `id` int NOT NULL,
+  `singer` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `band` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `titles` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `album` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `compositor` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_CD52224ABF396750` FOREIGN KEY (`id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `music`
+--
+
+LOCK TABLES `music` WRITE;
+/*!40000 ALTER TABLE `music` DISABLE KEYS */;
+/*!40000 ALTER TABLE `music` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address1` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address2` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `zipcode` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff`
+--
+
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_role`
+--
+
+DROP TABLE IF EXISTS `staff_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff_role` (
+  `staff_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`staff_id`,`role_id`),
+  KEY `IDX_B55FFCE5D4D57CD` (`staff_id`),
+  KEY `IDX_B55FFCE5D60322AC` (`role_id`),
+  CONSTRAINT `FK_B55FFCE5D4D57CD` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_B55FFCE5D60322AC` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_role`
+--
+
+LOCK TABLES `staff_role` WRITE;
+/*!40000 ALTER TABLE `staff_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `state_of_media`
+--
+
+DROP TABLE IF EXISTS `state_of_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `state_of_media` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `state_of_media`
+--
+
+LOCK TABLES `state_of_media` WRITE;
+/*!40000 ALTER TABLE `state_of_media` DISABLE KEYS */;
+INSERT INTO `state_of_media` VALUES (1,'mauvais état'),(2,'bon état'),(3,'neuf');
+/*!40000 ALTER TABLE `state_of_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stockable_media`
+--
+
+DROP TABLE IF EXISTS `stockable_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stockable_media` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `media_id` int NOT NULL,
+  `stock` int DEFAULT NULL,
+  `reception_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_7527D36AEA9FDD75` (`media_id`),
+  CONSTRAINT `FK_7527D36AEA9FDD75` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stockable_media`
+--
+
+LOCK TABLES `stockable_media` WRITE;
+/*!40000 ALTER TABLE `stockable_media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stockable_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stockable_media_copy`
+--
+
+DROP TABLE IF EXISTS `stockable_media_copy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stockable_media_copy` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `stockable_media_id` int NOT NULL,
+  `media_state_id` int NOT NULL,
+  `copy_number` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_98B66FD42C1EA804` (`stockable_media_id`),
+  KEY `IDX_98B66FD4AB91273` (`media_state_id`),
+  CONSTRAINT `FK_98B66FD42C1EA804` FOREIGN KEY (`stockable_media_id`) REFERENCES `stockable_media` (`id`),
+  CONSTRAINT `FK_98B66FD4AB91273` FOREIGN KEY (`media_state_id`) REFERENCES `state_of_media` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stockable_media_copy`
+--
+
+LOCK TABLES `stockable_media_copy` WRITE;
+/*!40000 ALTER TABLE `stockable_media_copy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stockable_media_copy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stream`
+--
+
+DROP TABLE IF EXISTS `stream`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stream` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `digital_media_id` int NOT NULL,
+  `member_id` int NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_F0E9BE1CA6AB6B0` (`digital_media_id`),
+  KEY `IDX_F0E9BE1C7597D3FE` (`member_id`),
+  CONSTRAINT `FK_F0E9BE1C7597D3FE` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
+  CONSTRAINT `FK_F0E9BE1CA6AB6B0` FOREIGN KEY (`digital_media_id`) REFERENCES `digital_media` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stream`
+--
+
+LOCK TABLES `stream` WRITE;
+/*!40000 ALTER TABLE `stream` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stream` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-15  7:33:53
