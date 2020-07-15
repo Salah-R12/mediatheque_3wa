dfcf4afbaa8a1cd5b37cd63e590b384a9b466858
Alper <alperakbulut@gmail.com>
2020-07-15 17:00:59 +0200

    modification

--- dumps/basic_inserts.sql
+++ dumps/basic_inserts.sql
@@ -1,6 +1,6 @@
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE TABLE `media_type`;
 INSERT INTO `media_type` VALUES (1,'Book',30),(2,'Film',7),(3,'Music',15);
-TRUNCATE TABLE `media_type`;
+TRUNCATE TABLE `state_of_media`;
 INSERT INTO `state_of_media` VALUES (1,'mauvais état'),(2,'bon état'),(3,'neuf');
 SET FOREIGN_KEY_CHECKS = 1;
