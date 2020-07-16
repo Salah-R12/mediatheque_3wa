ec63d6481aa79848cfb5336bf563ad06a837c6e5
malletgaetan <mallet.gaetan@hotmail.fr>
2020-07-16 01:44:11 -0700

    ajout des specifications de type pour les formulaires de MediaType et StateOfMedia

--- src/Form/MediaTypeType.php
+++ src/Form/MediaTypeType.php
@@ -6,14 +6,17 @@ use App\Entity\MediaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
 
 class MediaTypeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('borrow_duration')
+            ->add('name', TextType::class)
+            ->add('borrow_duration', NumberType::class)
         ;
     }
 
