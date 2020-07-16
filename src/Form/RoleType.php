eb7d2dd6e73b564529494b273cec6c9600e2cd2a
malletgaetan <mallet.gaetan@hotmail.fr>
2020-07-16 02:01:37 -0700

    staff crud

--- src/Form/RoleType.php
+++ src/Form/RoleType.php
@@ -6,14 +6,20 @@ use App\Entity\Role;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class RoleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('staffs')
+            ->add('name', TextType::class)
+            ->add('staffs', EntityType::class, [
+                'class' => Staff::class,
+                'choice_label' => 'username'
+            ])
         ;
     }
 
---
+++ src/Form/StaffType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Staff;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StaffType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('first_name')
+            ->add('last_name')
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('address1')
+            ->add('address2')
+            ->add('zipcode')
+            ->add('city')
+            ->add('phone')
+            ->add('roles')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Staff::class,
+        ]);
+    }
+}
