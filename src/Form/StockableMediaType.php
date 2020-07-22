d1e931fe1964833d9c3bd547f36b37db52bdb307
Fabrice D. <fabricedant@gmail.com>
2020-07-22 21:36:49 +0200

    définition plus fine des type de champs pour les livres et les "médias physiques"

--- src/Form/StockableMediaType.php
+++ src/Form/StockableMediaType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\StockableMedia;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class StockableMediaType extends AbstractType
@@ -12,8 +13,13 @@ class StockableMediaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('stock')
-            ->add('reception_date')
+            ->add('stock', null, ['label' => "Nombre d'exemplaires"])
+            ->add('reception_date', DateType::class, [
+            	'label' => 'Date de réception',
+            	'widget' => 'single_text',
+            	'html5' => true,
+            	'required' => false
+            ])
             //->add('media')
         ;
     }
