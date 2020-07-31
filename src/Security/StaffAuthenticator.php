ee0574586c97b28c096f226b32accf587e54ae8a
Fabrice D. <fabricedant@gmail.com>
2020-07-31 13:03:01 +0200

    Commentaires en plus et ajout d'une propriété "formSubmitted" dans les "Authenticator"

--- src/Security/StaffAuthenticator.php
+++ src/Security/StaffAuthenticator.php
@@ -39,6 +39,15 @@ class StaffAuthenticator extends AbstractFormLoginAuthenticator{
 
 	private $csrfTokenManager;
 
+	/**
+	 * Propriété ajoutée et mise à "false" par défaut, pour savoir dans quel contexte cette classe est utilisée, c'est-à-dire si on veut juste afficher la page de login ou
+	 * si le formulaire est soumis et que la classe est instanciée pour exécuter le programme d'authentification du user.
+	 * Donc, cette propriété sera passée à "true" dans la fonction "getCredentials"
+	 *
+	 * @var boolean
+	 */
+	private $formSubmitted = false;
+
 	public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager){
 		$this->entityManager = $entityManager;
 		$this->urlGenerator = $urlGenerator;
@@ -50,6 +59,12 @@ class StaffAuthenticator extends AbstractFormLoginAuthenticator{
 	}
 
 	public function getCredentials(Request $request){
+		// Puisque cette fonction "getCredentials" est appelée, cela signifie que le formulaire a été "posté" (soumis) et l'application va procéder à l'authentification
+		// avec les credentials saisis dans les inputs du formulaire (username et password)
+		// Cela nous est utile dans la fonction "getLoginUrl", car en cas d'erreur (par exemple, mauvais password ou username inconnu) on veut rester sur la page de login spécifique au type d'utilisateur
+		// et ne pas rebasculer vers la page principale de login qui présente les 2 formulaires de login car les messages d'erreur sont toujours affichés dans le premier block
+		$this->formSubmitted = true;
+		
 		$credentials = [
 			'username' => $request->request->get('username'),
 			'password' => $request->request->get('password'),
@@ -72,7 +87,7 @@ class StaffAuthenticator extends AbstractFormLoginAuthenticator{
 
 		if (!$user){
 			// fail authentication with a custom error
-			throw new CustomUserMessageAuthenticationException('Username could not be found.');
+			throw new CustomUserMessageAuthenticationException('Nom d\'utilisateur inconnu');
 		}
 
 		return $user;
@@ -89,18 +104,20 @@ class StaffAuthenticator extends AbstractFormLoginAuthenticator{
 		if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)){
 			return new RedirectResponse($targetPath);
 		}
-		
+
 		// Si aucun "targetPath" n'est passé (c'est dans le cas où la personne n'a pas tenté d'accéder à une page sécurisée avant de basculer vers la page de login, le système n'a donc pas mémorisé une URL valide pour se rediriger automatiquement)
 		// Alors on se redirige vers la dashboard par défaut
 		return new RedirectResponse($this->urlGenerator->generate('dashboard'));
 	}
 
 	protected function getLoginUrl(){
-		//return $this->urlGenerator->generate(self::LOGIN_ROUTE);
-
 		// NOTE FD: ici, on veut que lorsqu'on n'est pas encore connecté mais qu'on tente d'accéder à une page sécurisée (configurée dans security.yaml, dans la section "access_control")
 		//			on soit renvoyé vers la page de login principale (celle de LoginController, et non pas celle de SecurityStaffController) afin de permettre à l'utilisateur d'avoir le choix
 		//			de se connecter soit avec un profil member, soit un profil staff
-		return $this->urlGenerator->generate('login_index');
+		//			Mais lors de la soumission du formulaire, s'il y a une erreur, on veut afficher uniquement le formulaire qui a été soumis (et ignorer l'autre, donc)
+		//			Pour faire cela, on a ajouté et déclaré une propriété de classe "$formSumitted = false"
+		//			qui sera passé à "true" dans la fonction "getCredentials" (puisque cette fonction est exécutée au moment de la soumission du formulaire)
+		$route_name = $this->formSubmitted ? self::LOGIN_ROUTE : 'login_index';
+		return $this->urlGenerator->generate($route_name);
 	}
 }
